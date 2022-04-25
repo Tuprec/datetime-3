cd8350393f2213487f8335fa670fb8d0956b4178
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-04-25 08:55:57 +0200

    Add project files.

---
+++ datetime 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace datetime_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop = 1;
+            DateTime tijd = new DateTime();
+            do
+            {
+                Console.WriteLine(tijd.ToLongTimeString());
+                System.Threading.Thread.Sleep(1000);
+                tijd = tijd.AddSeconds(1);
+                Console.Clear();
+            } while (loop == 1);
+        }
+    }
+}
